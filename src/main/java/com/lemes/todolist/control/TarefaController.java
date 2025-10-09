7b1668806d67775871033e2560fa1ef8403679bd
Renan Laptop <renanmanoel15@gmail.com>
2025-10-09 19:20:14 -0300

    Post mapping

--- src/main/java/com/lemes/todolist/control/TarefaController.java
+++ src/main/java/com/lemes/todolist/control/TarefaController.java
@@ -1,12 +1,14 @@
 package com.lemes.todolist.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.lemes.todolist.model.Tarefa;
 import com.lemes.todolist.repository.TarefaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -16,9 +18,24 @@ public class TarefaController {
     @Autowired
     TarefaRepository rep;
 
-    @PostMapping("/")
-    public ResponseEntity(@RequestParam String param) {
+    
+    @GetMapping("/")
+    public ResponseEntity<List<Tarefa>> getAllTarefas(@RequestParam String param) {
         return new String();
+    }
+    
+
+    @PostMapping("/")
+    public ResponseEntity<Tarefa> createTarefa(@RequestBody Tarefa tar) {
+        try {
+            Tarefa t = rep.save(new Tarefa(tar.getDescricao(), tar.getPrioridade()));
+
+            return new ResponseEntity<>(t, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);            
+        }
+        
+        
     }
     
 }
